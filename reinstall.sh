e6c8a0e3cadc985b0a381424c68bf2a98a688827
Julian Dietlmeier <julian@dietlmeiers.de>
2021-12-08 11:59:24 +0100

    Update_2021-12-8

---
+++ reinstall.sh
@@ -0,0 +1,16 @@
+pacman -Syy
+pacman -Su
+pacman -S yay
+
+yay -S fakeroot base-devel binutils
+yay -S texlive-most texstudio
+
+yay -S bitwarden
+
+
+# recreate folder
+mkdir Documents
+mkdir Documents/Projects
+
+
+# setup dotfiles
